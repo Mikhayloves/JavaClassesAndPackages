3159a02952ca59209415e490a0a024a63da92311
Mikhaylovskiy Anton <toni_mikhaylovskiy@mail.ru>
2024-11-16 06:52:38 +0300

    Initial commit

---
+++ src/main/java/org/sberuniversity/Main.java
@@ -0,0 +1,12 @@
+package org.sberuniversity;
+
+import org.sberuniversity.family.Child;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Child child1 = new Child();
+        System.out.println();
+        Child child2 = new Child("Ivan");
+    }
+}
---
+++ src/main/java/org/sberuniversity/family/Child.java
@@ -0,0 +1,30 @@
+package org.sberuniversity.family;
+
+public class Child extends Parent {
+    static {
+        System.out.println("Child:static 1");
+    }
+
+    {
+        System.out.println("Child:instance 1");
+    }
+
+    static {
+        System.out.println("Child:static 2");
+    }
+
+    public Child() {
+        super();
+        System.out.println("Child:constructor");
+    }
+
+    public Child(String name) {
+        super(name);
+        System.out.println("Child:name-constructor");
+    }
+
+    {
+        System.out.println("Child:instance 2");
+    }
+}
+
